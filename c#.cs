ca7e336ea294b5e7e8d4a1715223810ff2d19b1f
Beeram Venkata Sasidhar Reddy <beeramvenkatasasidhar_reddy@epam.com>
2024-03-22 16:08:52 +0530

    committed

--- c#.cs
+++ c#.cs
@@ -4,11 +4,15 @@ namespace Demo
 {
     class HelloWorld
     {
+	public static int product(int a,int b){
+		return a*b;
+	}
 	public static int add(int a, int b){
 		return a+b;
 	}
         static void Main(string[] args){
             Console.WriteLine(add(10,20));
+	Console.WriteLine(add(10,20));
             Console.ReadLine();
         }
         
