ccc7441db539547123f1129500e5e2c3040e4a02
Beeram Venkata Sasidhar Reddy <beeramvenkatasasidhar_reddy@epam.com>
2024-03-22 15:51:51 +0530

    commit

---
+++ c#.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Demo
+{
+    class HelloWorld
+    {
+        static void Main(string[] args){
+            Console.WriteLine("helloworld");
+            Console.ReadLine();
+            
+
+        }
+        
+    }
+}
